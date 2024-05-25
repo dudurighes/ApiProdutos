b1f8ae86ad33537504e90838146b525385c6a31b
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2024-05-25 08:54:08 -0300

    Update produtoRoutes.ts

--- src/models/produto/routes/produtoRoutes.ts
+++ src/models/produto/routes/produtoRoutes.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance, RouteShorthandOptions } from "fastify";
 
 
-export const iesRoutes = (fastify: FastifyInstance,
+export const produtosRoutes = (fastify: FastifyInstance,
     options: RouteShorthandOptions, done: () => void
 ) => {
     fastify.register()
